f0b37e6f84ea28693fb726164cc2f5a98bcf28f4
Tyler <tsmith328@gatech.edu>
2016-03-03 13:07:43 -0500

    New Project Wizard Complete
    Now creates source folder and changes perspectives

--- org.team38.assembly.parent/org.team38.assembly/src/org/team38/assembly/wizards/LC2200ProjectNewWizard.java
+++ org.team38.assembly.parent/org.team38.assembly/src/org/team38/assembly/wizards/LC2200ProjectNewWizard.java
@@ -1,14 +1,26 @@
 package org.team38.assembly.wizards;
 
+import java.net.URI;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
-public class LC2200ProjectNewWizard extends Wizard implements INewWizard {
+public class LC2200ProjectNewWizard extends Wizard implements INewWizard, IExecutableExtension {
 	
 	private WizardNewProjectCreationPage pageOne;
+	private IConfigurationElement configElement;
 
 	public LC2200ProjectNewWizard() {
 		setWindowTitle("Window Title");
@@ -24,12 +36,16 @@ public class LC2200ProjectNewWizard extends Wizard implements INewWizard {
 	@Override
 	public boolean performFinish() {
 		String projectName = pageOne.getProjectName();
+		URI location = null;
 		if (projectName == null || projectName.length() == 0) {
 			return false;
 		}
-		if(pageOne.useDefaults()) {
-			return true;
+		if(!pageOne.useDefaults()) {
+			location = pageOne.getLocationURI();
 		}
+		createProject(projectName, location);
+		
+		BasicNewProjectResourceWizard.updatePerspective(configElement);
 		return true;
 	}
 	
@@ -43,4 +59,60 @@ public class LC2200ProjectNewWizard extends Wizard implements INewWizard {
 
         addPage(pageOne);
     }
+
+	@Override
+	public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
+			throws CoreException {
+		// TODO Auto-generated method stub
+		configElement = config;
+		
+	}
+	
+	private IProject createProject(String projectName, URI location) {
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+		if(!project.exists()) {
+			URI projectLocation = location;
+			IProjectDescription desc = project.getWorkspace().newProjectDescription(project.getName());
+			if (location != null && ResourcesPlugin.getWorkspace().getRoot().getLocationURI().equals(location)) {
+				projectLocation = null;
+			}
+			
+			desc.setLocationURI(projectLocation);
+			try {
+				project.create(desc,null);
+				if(!project.isOpen()) {
+					project.open(null);
+				}
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		try {
+			String[] paths = { "src" };
+			addToProjectStructure(project, paths);
+		} catch (CoreException e) {
+			e.printStackTrace();
+			project = null;
+		}
+		
+		return project;
+	}
+	
+	private void addToProjectStructure(IProject project, String[] paths) throws CoreException {
+		for (String path : paths) {
+			IFolder etcFolders = project.getFolder(path);
+			createFolder(etcFolders);
+		}
+	}
+	
+	private void createFolder(IFolder folder) throws CoreException {
+		IContainer parent = folder.getParent();
+		if (parent instanceof IFolder) {
+			createFolder((IFolder) parent);
+		}
+		if (!folder.exists()) {
+			folder.create(false, true, null);
+		}
+	}
 }
